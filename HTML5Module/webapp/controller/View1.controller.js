7cca065875d5eadf9a90df87c1097d17e9b5ff71
AlexandreCordaJunior <alexandre.corda@fatec.sp.gov.br>
2021-01-25 12:51:44 +0000

    first commit

---
+++ HTML5Module/webapp/controller/View1.controller.js
@@ -0,0 +1,23 @@
+// @ts-nocheck
+sap.ui.define([
+		"sap/ui/core/mvc/Controller"
+	],
+	/**
+     * @param {typeof sap.ui.core.mvc.Controller} Controller
+     */
+	function (Controller) {
+		"use strict";
+
+		return Controller.extend("ns.HTML5Module.controller.View1", {
+			onInit: function () {
+                /*$.ajax("/upload/129858a0-5c86-48b8-9767-1e2f936c7dc4", {
+                    success: function(data) {
+                        console.log(data);
+                    },
+                    error: function(error) {
+                        console.log(error);
+                    }
+                });*/
+			}
+		});
+	});
